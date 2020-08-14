3e250bd59830f30fb4d45b24b70967734a17420a
David Keller <mrcodeswildride@gmail.com>
2020-08-14 18:29:53 +0000

    es6 update

--- script.js
+++ script.js
@@ -1,242 +1,209 @@
-var squares = document.querySelectorAll(".square");
-var message = document.getElementById("message");
-var x = document.getElementById("x");
-var o = document.getElementById("o");
-var restartButton = document.getElementById("restart");
-var switchButton = document.getElementById("switch");
+let squares = document.getElementsByClassName(`square`)
+let messageParagraph = document.getElementById(`messageParagraph`)
+let xParagraph = document.getElementById(`xParagraph`)
+let oParagraph = document.getElementById(`oParagraph`)
+let restartButton = document.getElementById(`restartButton`)
+let switchButton = document.getElementById(`switchButton`)
 
-var ai = "O";
-var turn = "X";
-var gameOver = false;
+let ai = `O`
+let turn = `X`
+let gameOver = false
 
-for (var i = 0; i < 9; i++) {
-    squares[i].addEventListener("click", clickSquare);
+for (let square of squares) {
+  square.addEventListener(`click`, clickSquare)
 }
 
-restartButton.addEventListener("click", restart);
-switchButton.addEventListener("click", switchLetters);
+restartButton.addEventListener(`click`, restart)
+switchButton.addEventListener(`click`, switchLetters)
 
 function clickSquare() {
-    if (turn != ai && !gameOver && this.innerHTML == "") {
-        this.innerHTML = turn;
-        turn = turn == "X" ? "O" : "X";
-
-        checkGameOver();
-
-        if (!gameOver) {
-            setTimeout(aiPlaceLetter, 10);
-        }
-    }
+  if (this.innerHTML == `` && turn != ai && !gameOver) {
+    this.innerHTML = turn
+    switchTurn()
+  }
 }
 
-function restart() {
-    turn = "X";
-    gameOver = false;
+function switchTurn() {
+  let board = getBoard()
 
-    for (var i = 0; i < 9; i++) {
-        squares[i].innerHTML = "";
+  if (threeInRow(board, turn)) {
+    messageParagraph.innerHTML = `${turn} wins!`
+    gameOver = true
+  }
+  else if (boardIsFull(board)) {
+    messageParagraph.innerHTML = `Tie game`
+    gameOver = true
+  }
+  else {
+    turn = turn == `X` ? `O` : `X`
+
+    if (turn == ai) {
+      setTimeout(aiMove, 10)
     }
-
-    message.innerHTML = "";
-
-    if (ai == turn) {
-        setTimeout(aiPlaceLetter, 10);
-    }
+  }
 }
 
-function switchLetters() {
-    ai = ai == "X" ? "O" : "X";
-    x.innerHTML = ai == "X" ? "Computer is X" : "You are X";
-    o.innerHTML = ai == "O" ? "Computer is O" : "You are O";
-    switchButton.innerHTML = ai == "X" ? "Let yourself go first" : "Let computer go first";
+function aiMove() {
+  let highestScore = -Infinity
+  let squareIndexes
 
-    restart();
+  let board = getBoard()
+  let nextBoards = getNextBoards(board, turn)
+
+  for (let nextBoard of nextBoards) {
+    let score = getScore(nextBoard, turn, true)
+    let squareIndex = getSquareIndex(board, nextBoard)
+
+    if (score > highestScore) {
+      highestScore = score
+      squareIndexes = [squareIndex]
+    }
+    else if (score == highestScore) {
+      squareIndexes.push(squareIndex)
+    }
+  }
+
+  let squareIndex = squareIndexes[Math.floor(Math.random() * squareIndexes.length)]
+  squares[squareIndex].innerHTML = turn
+  switchTurn()
 }
 
-function checkGameOver() {
-    var board = getBoard();
+function getScore(board, currentTurn, aiTurn) {
+  if (threeInRow(board, currentTurn)) {
+    return aiTurn ? 1 : -1
+  }
+  else if (boardIsFull(board)) {
+    return 0
+  }
+  else {
+    let nextTurn = currentTurn == `X` ? `O` : `X`
+    let nextBoards = getNextBoards(board, nextTurn)
 
-    if (threeInRow(board, "X")) {
-        gameOver = true;
-        message.innerHTML = "X wins!";
-    }
-    else if (threeInRow(board, "O")) {
-        gameOver = true;
-        message.innerHTML = "O wins!";
-    }
-    else if (boardIsFull(board)) {
-        gameOver = true;
-        message.innerHTML = "Tie game";
-    }
-}
+    if (aiTurn) {
+      let strongestScore = Infinity
 
-function threeInRow(board, letter) {
-    if (board[0] == letter && board[1] == letter && board[2] == letter) {
-        return true;
-    }
-    else if (board[3] == letter && board[4] == letter && board[5] == letter) {
-        return true;
-    }
-    else if (board[6] == letter && board[7] == letter && board[8] == letter) {
-        return true;
-    }
-    else if (board[0] == letter && board[3] == letter && board[6] == letter) {
-        return true;
-    }
-    else if (board[1] == letter && board[4] == letter && board[7] == letter) {
-        return true;
-    }
-    else if (board[2] == letter && board[5] == letter && board[8] == letter) {
-        return true;
-    }
-    else if (board[0] == letter && board[4] == letter && board[8] == letter) {
-        return true;
-    }
-    else if (board[2] == letter && board[4] == letter && board[6] == letter) {
-        return true;
-    }
+      for (let nextBoard of nextBoards) {
+        let score = getScore(nextBoard, nextTurn, false)
+        strongestScore = Math.min(score, strongestScore)
+      }
 
-    return false;
-}
-
-function boardIsFull(board) {
-    for (var i = 0; i < board.length; i++) {
-        if (board[i] == "") {
-            return false;
-        }
-    }
-
-    return true;
-}
-
-function aiPlaceLetter() {
-    var selectedSquareIndex = null;
-    var board = getBoard();
-
-    if (boardIsEmpty(board)) {
-        selectedSquareIndex = 4;
+      return strongestScore
     }
     else {
-        var highestScore = -Infinity;
-        var highestScoreIndexes = [];
-        var nextBoards = getNextBoards(board, turn == "X" ? "O" : "X");
+      let strongestScore = -Infinity
 
-        for (var i = 0; i < nextBoards.length; i++) {
-            var score = getScore(nextBoards[i], turn, true);
+      for (let nextBoard of nextBoards) {
+        let score = getScore(nextBoard, nextTurn, true)
+        strongestScore = Math.max(score, strongestScore)
+      }
 
-            if (score > highestScore) {
-                highestScore = score;
-                highestScoreIndexes = [i];
-            }
-            else if (score == highestScore) {
-                highestScoreIndexes.push(i);
-            }
-        }
-
-        var squareIndexes = [];
-
-        for (var i = 0; i < highestScoreIndexes.length; i++) {
-            var squareIndex = getSquareIndex(board, nextBoards[highestScoreIndexes[i]]);
-            squareIndexes.push(squareIndex);
-
-            if (squareIndex == 4) {
-                selectedSquareIndex = 4;
-            }
-        }
-
-        if (selectedSquareIndex == null) {
-            selectedSquareIndex = squareIndexes[Math.floor(Math.random() * squareIndexes.length)];
-        }
+      return strongestScore
     }
-
-    squares[selectedSquareIndex].innerHTML = turn;
-    turn = turn == "X" ? "O" : "X";
-
-    checkGameOver();
+  }
 }
 
 function getBoard() {
-    var board = [];
+  let board = []
 
-    for (var i = 0; i < 9; i++) {
-        board.push(squares[i].innerHTML);
-    }
+  for (let square of squares) {
+    board.push(square.innerHTML)
+  }
 
-    return board;
+  return board
 }
 
-function boardIsEmpty(board) {
-    for (var i = 0; i < 9; i++) {
-        if (board[i] != "") {
-            return false;
-        }
+function getNextBoards(board, currentTurn) {
+  let nextBoards = []
+
+  for (let i = 0; i < board.length; i++) {
+    if (board[i] == ``) {
+      let boardCopy = board.slice()
+      boardCopy[i] = currentTurn
+
+      nextBoards.push(boardCopy)
     }
+  }
 
-    return true;
-}
-
-function getNextBoards(board, turn) {
-    var nextBoards = [];
-    var nextTurn = turn == "X" ? "O" : "X";
-
-    for (var i = 0; i < 9; i++) {
-        var nextBoard = board.slice();
-
-        if (nextBoard[i] == "") {
-            nextBoard[i] = nextTurn;
-            nextBoards.push(nextBoard);
-        }
-    }
-
-    return nextBoards;
+  return nextBoards
 }
 
 function getSquareIndex(board1, board2) {
-    for (var i = 0; i < 9; i++) {
-        if (board1[i] != board2[i]) {
-            return i;
-        }
+  for (let i = 0; i < board1.length; i++) {
+    if (board1[i] != board2[i]) {
+      return i
     }
+  }
 
-    return -1;
+  return -1
 }
 
-function getScore(board, turn, aiMove) {
-    if (threeInRow(board, "X")) {
-        return turn == "X" ? (aiMove ? 1 : -1) : (aiMove ? -1 : 1);
+function restart() {
+  turn = `X`
+  gameOver = false
+
+  for (let square of squares) {
+    square.innerHTML = ``
+  }
+
+  messageParagraph.innerHTML = ``
+
+  if (turn == ai) {
+    setTimeout(aiMove, 10)
+  }
+}
+
+function switchLetters() {
+  ai = ai == `X` ? `O` : `X`
+
+  if (ai == `X`) {
+    xParagraph.innerHTML = `Computer is X`
+    oParagraph.innerHTML = `You are O`
+    switchButton.innerHTML = `Let yourself go first`
+  }
+  else {
+    xParagraph.innerHTML = `You are X`
+    oParagraph.innerHTML = `Computer is O`
+    switchButton.innerHTML = `Let computer go first`  
+  }
+
+  restart()
+}
+
+function threeInRow(board, currentTurn) {
+  if (board[0] == currentTurn && board[1] == currentTurn && board[2] == currentTurn) {
+    return true
+  }
+  else if (board[3] == currentTurn && board[4] == currentTurn && board[5] == currentTurn) {
+    return true
+  }
+  else if (board[6] == currentTurn && board[7] == currentTurn && board[8] == currentTurn) {
+    return true
+  }
+  else if (board[0] == currentTurn && board[3] == currentTurn && board[6] == currentTurn) {
+    return true
+  }
+  else if (board[1] == currentTurn && board[4] == currentTurn && board[7] == currentTurn) {
+    return true
+  }
+  else if (board[2] == currentTurn && board[5] == currentTurn && board[8] == currentTurn) {
+    return true
+  }
+  else if (board[0] == currentTurn && board[4] == currentTurn && board[8] == currentTurn) {
+    return true
+  }
+  else if (board[2] == currentTurn && board[4] == currentTurn && board[6] == currentTurn) {
+    return true
+  }
+
+  return false
+}
+
+function boardIsFull(board) {
+  for (let cell of board) {
+    if (cell == ``) {
+      return false
     }
-    else if (threeInRow(board, "O")) {
-        return turn == "O" ? (aiMove ? 1 : -1) : (aiMove ? -1 : 1);
-    }
-    else if (boardIsFull(board)) {
-        return 0;
-    }
-    else {
-        var nextBoards = getNextBoards(board, turn);
-        var nextTurn = turn == "X" ? "O" : "X";
+  }
 
-        var score;
-        var strongestScore;
-
-        if (aiMove) {
-            strongestScore = Infinity;
-
-            for (var i = 0; i < nextBoards.length; i++) {
-                score = getScore(nextBoards[i], nextTurn, false);
-                strongestScore = Math.min(score, strongestScore);
-            }
-
-            return strongestScore;
-        }
-        else {
-            strongestScore = -Infinity;
-
-            for (var i = 0; i < nextBoards.length; i++) {
-                score = getScore(nextBoards[i], nextTurn, true);
-                strongestScore = Math.max(score, strongestScore);
-            }
-
-            return strongestScore;
-        }
-    }
+  return true
 }
